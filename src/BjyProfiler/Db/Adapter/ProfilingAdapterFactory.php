59a39388c652562358453adaee2594cddcd6c913
APaikens <shamanis@gmail.com>
2019-02-12 23:14:57 +0200

    $serviceLocator->$container

--- src/BjyProfiler/Db/Adapter/ProfilingAdapterFactory.php
+++ src/BjyProfiler/Db/Adapter/ProfilingAdapterFactory.php
@@ -20,7 +20,7 @@ class ProfilingAdapterFactory implements FactoryInterface
     
     
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null) {
-        $config = $serviceLocator->get('Configuration');
+        $config = $container->get('config');
         $dbParams = $config['db'];
         $adapter = new ProfilingAdapter($dbParams);
 
