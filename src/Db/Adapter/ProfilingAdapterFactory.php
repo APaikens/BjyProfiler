59a1d8c79ca2f993499184deb89c5643d918b646
Andris Paikens <Andris.Paikens@di.lv>
2020-03-19 08:57:31 +0200

    add missing files

---
+++ src/Db/Adapter/ProfilingAdapterFactory.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by Inditel Meedia OÜ
+ * User: Oliver Leisalu
+ */
+
+namespace BjyProfiler\Db\Adapter;
+
+
+use BjyProfiler\Db\Profiler\Profiler;
+use Laminas\ServiceManager\FactoryInterface;
+use Laminas\ServiceManager\ServiceLocatorInterface;
+use \Interop\Container\ContainerInterface;
+
+class ProfilingAdapterFactory implements FactoryInterface
+{
+    public function createService(ServiceLocatorInterface $serviceLocator) {
+        return $this->__invoke($serviceLocator,null);
+    }
+    
+    
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null) {
+        $config = $container->get('config');
+        $dbParams = $config['db'];
+        $adapter = new ProfilingAdapter($dbParams);
+
+        $adapter->setProfiler(new Profiler);
+        if (isset($dbParams['options']) && is_array($dbParams['options'])) {
+            $options = $dbParams['options'];
+        } else {
+            $options = array();
+        }
+        $adapter->injectProfilingStatementPrototype($options);
+        return $adapter;
+    }
+  
+}
